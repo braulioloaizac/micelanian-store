513f5d3b5f33c87188a270c79cec26e2ff127d42
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-07-31 20:45:15 -0400

    working all models and seeding correctly

---
+++ models/ProductTag.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'product_id'
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'tag_id'
+      },
+    },
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
